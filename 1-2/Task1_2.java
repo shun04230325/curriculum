3884fa01d058ab491f93c3ff6d16b29a43e49c57
shun <mizuki33.1126@gmail.com>
2026-06-18 16:31:51 +0900

    お米炊いて冷凍しておきます

--- 1-2/Task1_2.java
+++ 1-2/Task1_2.java
@@ -3,24 +3,25 @@ public class Task1_2 {
     public static void main(String[] args) {
 
         // ①
-
+        int hogeInt = 50;
         // ②
-
+        hogeInt = 100;
         // ③
-
+        String hogeString = "テスト";
         // ④
-
+        hogeString = "合格";
         // ⑤
-        // [コメントをここへ記述]
+
+        // doouble型のhogeDoubleという名前の変数を宣言し、値「0.2」で初期化しています。
         double hogeDouble = 0.2;
 
         // ⑥
-
+        System.out.println(hogeInt);
         // ⑦
-        // [コメントをここへ記述]
+        // 変数hogeStringの値を表示します。
         System.out.println(hogeString);
 
         // ⑧
-
+        System.out.println(hogeDouble);
     }
 }
