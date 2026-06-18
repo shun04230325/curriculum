6dc33ce1d1909c3849e1bb6ac5ac7690a6f7d248
shun <mizuki33.1126@gmail.com>
2026-06-18 19:48:10 +0900

    おかえりなさい！

--- 1-5-2/Task1_5_2.java
+++ 1-5-2/Task1_5_2.java
@@ -1,9 +1,39 @@
 public class Task1_5_2 {
 
     public static void main(String[] args) {
-        String button = "□";
+        String button = "□＋△";
 
-        switch () {
+        switch (button) {
+             case "□":
+                System.out.println("パンチ！");
+                break;
+
+            case "△":
+                System.out.println("キック！");
+                break;
+
+            case "〇":
+                System.out.println("しゃがむ");
+                break;
+
+            case "×":
+                System.out.println("前転");
+                break;
+
+            case "R":
+                System.out.println("ガード");
+                break;
+
+            case "L":
+                System.out.println("ガード");
+                break;
+
+            case "□＋△":
+                System.out.println("パンチ");
+                System.out.println("キック");
+                break;
+
+            
 
         }
     }
