887dfb4ec3721c0276f3646095c4554c256b79e0
shun <mizuki33.1126@gmail.com>
2026-06-20 18:05:55 +0900

    でけた！！

--- 1-6-2/Task1_6_2.java
+++ 1-6-2/Task1_6_2.java
@@ -1,6 +1,14 @@
 public class Task1_6_2 {
 
     public static void main(String[] args) {
+ for (int i = 1; i <= 9; i++) {
+     System.out.print(i  + " ||");
+            
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(" " + i  * j + " |");
+            }
+            System.out.println();
+        }
 
     }
 }
