2779fe0bdeb6c914a88c8b07d7421a93467b2f5c
shun <mizuki33.1126@gmail.com>
2026-08-08 17:36:04 +0900

    オーバーロード

--- 2-05/src/question1/Calculator.java
+++ 2-05/src/question1/Calculator.java
@@ -3,17 +3,17 @@ package question1;
 public class Calculator {
 
     // ①
-    public static int plus() {
-
+    public static int plus(int a) {
+        return a + 1;
     }
 
     // ②
-    public static int plus() {
-
+    public static int plus(int a, int b) {
+        return a + b;
     }
 
     // ③
-    public static int plus() {
-
+    public static int plus(int a, int b, int c) {
+        return a + b + c;
     }
 }
--- 2-05/src/question1/Main.java
+++ 2-05/src/question1/Main.java
@@ -1,11 +1,17 @@
 package question1;
 
 // ①
-public class Main {
+
+public class Main extends Calculator {
 
     public static void main(String[] args) {
 
         // ②
+        System.out.println("plusメソッドの引数が1つ:" + Calculator.plus(10));
+        System.out.println("plusメソッドの引数が2つ:" + Calculator.plus(10, 20));
+        System.out.println("plusメソッドの引数が3つ:" + Calculator.plus(10, 20, 30));
+        
+       
 
     }
 }
