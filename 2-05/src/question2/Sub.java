00057e10fa0fbb7273c520bf8095e3c5b8536e69
shun <mizuki33.1126@gmail.com>
2026-08-13 23:52:13 +0900

    5-2-2

--- 2-05/src/question2/Sub.java
+++ 2-05/src/question2/Sub.java
@@ -3,11 +3,18 @@ package question2;
 public class Sub extends Super{
 
     // ① 
-
+    protected void show () {
+        System.out.println("サブクラスのメソッドです。");
+    }
     // ② 
+    protected void introduction() {
+        show();
+        super.show();
+    }
 
     public static void main(String[] args) {
         // ③ 
-
+        Sub mj = new Sub();
+        mj.introduction();
     }
 }
